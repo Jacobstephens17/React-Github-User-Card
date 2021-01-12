67d37b5bc3c491410358d9a07dccf8096517ed9e
Jacob Stephens <jacobstephens2@gmail.com>
2021-01-12 14:41:40 -0800

    finished basic styling

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import './App.css';
+import styled from 'styled-components'
 
 class App extends React.Component {
 
@@ -25,7 +25,7 @@ componentDidMount() {
       location: res.data.location,
       name: res.data.name,
       repos: res.data.public_repos,
-      url: res.data.url
+      url: res.data.html_url
     })
     console.log(this.state)
   })
@@ -38,21 +38,57 @@ componentDidMount() {
     
     return (
       <div className="App">
-        <h1>Test</h1>
-        <div>
-          <img src={this.state.photo} />
-          <h1>Name: {this.state.name}</h1>
-          <p>Bio: {this.state.bio}</p>
-          <h4>Followers: {this.state.followers}</h4>
-          <h4>Repos: {this.state.repos}</h4>
-          <h4>Location: {this.state.location}</h4>
-          <p>Profile Url: {this.state.url}</p>
-
-
-        </div>
+        <h1>GitHub User Cards</h1>
+        <StyledCard>
+          <Img src={this.state.photo} />
+          <StyledContent>
+            <h1>Name: {this.state.name}</h1>
+            <p>Bio: {this.state.bio}</p>
+            <h4>Followers: {this.state.followers}</h4>
+            <h4>Repos: {this.state.repos}</h4>
+            <h4>Location: {this.state.location}</h4>
+            <Button href={this.state.url}>Link To GitHub Profile</Button>
+          </StyledContent>
+        </StyledCard>
       </div>
     );
   }
 }
 
 export default App;
+
+
+const StyledCard = styled.div`
+display:flex;
+flex-direction:row;
+align-items:center;
+padding:2rem;
+background-color:black;
+color:white;
+margin:0 auto;
+justify-content:center;
+padding:8rem;
+`
+
+const StyledContent = styled.div`
+display:flex;
+flex-direction:column;
+padding:0;
+margin:0;
+padding-left:2rem;
+`
+
+const Img = styled.img`
+width:350px;
+border-radius:6px;
+
+`
+
+const Button = styled.button`
+background:none;
+border:white 1px solid;
+border-radius:6px;
+color:white;
+padding:1rem 1rem;
+width:50%;
+`
